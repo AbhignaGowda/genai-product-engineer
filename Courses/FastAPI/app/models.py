0f6593e65d8453937adfa90f533aace4055e69fa
Abhigna <abhignagowda07@gmail.com>
2025-08-10 14:03:45 +0530

    fastapi with postgresql

--- Courses/FastAPI/app/models.py
+++ Courses/FastAPI/app/models.py
@@ -1,4 +1,5 @@
 
+import string
 from sqlalchemy import Column , Integer ,String, Boolean,TIMESTAMP
 from sqlalchemy.sql.expression import text
 from .database import Base
@@ -12,3 +13,9 @@ class Post(Base):
     content=Column(String,nullable=False)
     published=Column(Boolean,server_default='True',nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text(('now()')))
+
+class User(Base):
+    __tablename__="users"
+    id = Column(Integer,primary_key=True, nullable=False)
+    email= Column(String,nullable=False,unique=True)
+    password=Column(String,nullable=False)
